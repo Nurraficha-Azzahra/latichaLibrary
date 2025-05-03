3a25d2e8fb1a641076cbc04f253f68d870ffe724
marcheladewii <marchela.dewi6@smk.belajar.id>
2025-05-03 20:56:50 +0700

    update UI footer

--- blog/footer.php
+++ blog/footer.php
@@ -1,8 +1,8 @@
 </main><!-- End #main -->
 <!-- ======= Footer ======= -->
-<footer id="footer" class="footer">
+<footer id="footer" class="footer" >
   <div class="footer-top">
-    <div class="container">
+    <div class="container" >
       <div class="row gy-4">
         <div class="col-lg-5 col-md-12 footer-info">
           <a href="index.html" class="logo d-flex align-items-center">
